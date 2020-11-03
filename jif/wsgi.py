b8f6065126144b9627c4c750d34a8d8c03650646
Tiago da Silva Valeriano de Oliveira <tiago.svo@gmail.com>
2020-11-03 13:26:18 -0300

    Mudança do nome do app principal do projeto de jifb para jif

--- jif/wsgi.py
+++ jif/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for jifb project.
+WSGI config for jif project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jifb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jif.settings')
 
 application = get_wsgi_application()

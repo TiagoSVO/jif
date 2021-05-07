6fcf0158972713b118498c7e71adc84704a2e3b4
TiagoSVO <tiago.svo@gmail.com>
2021-05-07 13:49:30 -0300

    [IMPROVEMENT] Adicionado restrição para add/update/delete related_field em Events

--- core/admin.py
+++ core/admin.py
@@ -29,7 +29,17 @@ class JIFAdmin(admin.ModelAdmin):
 
 @admin.register(JIFsEvent)
 class JIFsEventAdmin(admin.ModelAdmin):
-    pass
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(JIFsEventAdmin, self).get_form(request, obj, **kwargs)
+        disable_related_crud = ['jif']
+
+        for field_name in disable_related_crud:
+            field = form.base_fields[field_name]
+            field.widget.can_add_related = False
+            field.widget.can_change_related = False
+            field.widget.can_delete_related = False
+        return form
 
 
 @admin.register(Committee)
